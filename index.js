7e675c52f3e05e39741f9fa5185b35e7b1d6b085
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-06-16 17:31:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+// map functions
+const mapToNegativize = (arr, function) => {
+  return arr.map(item => item);
+};
+console.log(mapToNegativize([1, 2, 3, -9], function(a){ return a * a }));
+
+// function mapToNegativize(arr, function(a) {
+//   arr.map(item => item)
+// }, " Gary")
+
+const mapToNoChange = (arr) => {
+  return arr.map(item => item);
+};
+console.log(mapToNoChange([1,2,3]));
+
+const mapToDouble = (arr) => {
+  return arr.map(item => item * 2);
+};
+console.log(mapToDouble([1,2,3]));
+
+const mapToSquare = (arr) => {
+  return arr.map(item => item * item);
+};
+console.log(mapToSquare([1,2,3]));
+
+
+
+
